e657616440602d332d27be5141ebb1b9b931013a
LeifChen <leifchen90@gmail.com>
2018-12-07 14:12:34 +0800

    chore: init project

---
+++ README.md
@@ -0,0 +1,15 @@
+# Hello-SpringBoot
+
+整理常用 Spring Boot 相关的知识内容。
+
+## 多环境配置
+
+- 配置开发环境：application-dev.yml
+- 配置正式环境：application-prd.yml
+
+借由配置文件 application.yml 中的 spring.profiles.active=dev 来实现环境的选择。
+
+## 参考
+
+1. [Spring Boot 2.0深度实践-初遇Spring Boot](https://www.imooc.com/learn/933)
+2. [Spring Boot进阶之Web进阶](https://www.imooc.com/learn/810)
---
+++ src/main/java/com/chen/App.java
@@ -0,0 +1,18 @@
+package com.chen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 应用程序入口
+ *
+ * @Author LeifChen
+ * @Date 2018-11-13
+ */
+@SpringBootApplication
+public class App {
+
+    public static void main(String[] args) {
+        SpringApplication.run(App.class, args);
+    }
+}
